946816f69809ddd23de14e7b54abec37c3f3b766
nghidali <natalie@ghidali.com>
2020-04-18 17:30:15 -0700

    Fixed unicode error and changed CanvasRecommender script to filter out users who have not filled the relevant canvas sections

--- ipm-finished-modules/CanvasRecommender.py
+++ ipm-finished-modules/CanvasRecommender.py
@@ -1,31 +1,31 @@
 import json
 import sys
-import pdb
+
 
 def recommend(section_query):
     with open("canvasSections.json") as canvas_json:
         canvas_map = json.load(canvas_json)
         search_dict = {}
         for user in canvas_map:
-            name = unicode(user["name"])
+            name = user["name"]
             sections = user["sections"][0]
             for section in sections:
                 for prompt_list in sections[section]:
-                    # pdb.set_trace()
-                    cur_key = unicode(section) + " --> " + unicode(prompt_list["prompt"])
+                    cur_key = section + " --> " + prompt_list["prompt"]
                     if cur_key in search_dict:
-                        search_dict[cur_key].append((name, unicode(prompt_list["status"])))
+                        search_dict[cur_key].append((name, prompt_list["status"]))
                     else:
-                        search_dict[cur_key] = [(name, unicode(prompt_list["status"]))]
+                        search_dict[cur_key] = [(name, prompt_list["status"])]
         if section_query in search_dict:
             result = search_dict[section_query]
             for helper in result:
-                print("Helper: " + helper[0] + " (" + helper[1] + ')')
+                if helper[1] != "Unfilled":
+                    print("Helper: " + helper[0] + " (" + helper[1] + ')')
         else:
             print("Did not find section")
     return
 
+
 if __name__ == '__main__':
-    # pdb.set_trace()
     query = ' '.join(sys.argv[1:])
     recommend(query)
